9be5ee2c720573841f6f4e976fee0f61afcb0232
KiwiCoder0100 <127875924+KiwiCoder0100@users.noreply.github.com>
2023-04-17 22:57:26 +1200

    Now using Azure & XML

--- ghostgpt.py
+++ ghostgpt.py
@@ -1,41 +1,65 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog, ttk
 import speech_recognition as sr
-import pyttsx3
+import azure.cognitiveservices.speech as speechsdk
 import openai
 import os
-from tkinter import filedialog
 import webbrowser
+import xml.etree.ElementTree as ET
+from azure.cognitiveservices.speech import SpeechConfig, SpeechSynthesizer, AudioConfig
+from azure.core.credentials import AzureKeyCredential
+
 
 def open_github_link(event):
     webbrowser.open("https://github.com/ghostkiwicoder")
 
+
 def create_and_open_api_key_file():
-    api_key_file = "apikey.txt"
+    api_key_file = "apikey.xml"
     if not os.path.isfile(api_key_file):
-        with open(api_key_file, "w") as f:
-            f.write("your-api-key")
+        root = ET.Element("config")
+        ET.SubElement(root, "azure_api_key").text = "your-azure-api-key"
+        ET.SubElement(root, "azure_region").text = "your-azure-region"
+        ET.SubElement(root, "openai_api_key").text = "your-openai-api-key"  # Add this line
+
+        tree = ET.ElementTree(root)
+        tree.write(api_key_file)
+
     os.startfile(api_key_file)
-    check_api_key_file()  # Add this line to check the API key after opening the file
+    check_api_key_file()
+
 
 def check_api_key_file():
-    api_key_file = "apikey.txt"
+    api_key_file = "apikey.xml"
     if not os.path.isfile(api_key_file):
         api_key_status.config(text="Please load your API key", fg="red")
     else:
-        with open(api_key_file, "r") as f:
-            content = f.read().strip()
-        if content and content != "your-api-key":
-            openai.api_key = content
+        tree = ET.parse(api_key_file)
+        root = tree.getroot()
+
+        azure_api_key = root.find("azure_api_key").text.strip() if root.find("azure_api_key") is not None else ""
+        azure_region = root.find("azure_region").text.strip() if root.find("azure_region") is not None else ""
+        openai_api_key = root.find("openai_api_key").text.strip() if root.find("openai_api_key") is not None else ""
+
+        if (azure_api_key and azure_api_key != "your-azure-api-key" and
+                azure_region and azure_region != "your-azure-region" and
+                openai_api_key and openai_api_key != "your-openai-api-key"):
             api_key_status.config(text="API Key Ready", fg="green")
+            load_voices(azure_api_key, azure_region)
+            openai.api_key = openai_api_key
         else:
             api_key_status.config(text="Please load your API key", fg="red")
+
+
+
 
 def export_chat():
     file_path = filedialog.asksaveasfilename(defaultextension=".txt")
     if file_path:
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(chat_log.get(1.0, "end"))
+
 
 def talk_to_gpt(prompt):
     response = openai.Completion.create(
@@ -47,6 +71,7 @@ def talk_to_gpt(prompt):
         temperature=0.5,
     )
     return response.choices[0].text.strip()
+
 
 def send_message():
     message = entry.get()
@@ -64,8 +89,9 @@ def send_message():
     chat_log.tag_add("gpt", chat_log.index("end - 2 lines linestart"), chat_log.index("end - 2 lines lineend"))
     chat_log.see("end")
     chat_log.config(state="disabled")
-    print(response)  # Print the response to the console before speaking
+    print(response)
     speak(response)
+
 
 def listen_to_user():
     start_button.config(text="Listening...")
@@ -82,16 +108,40 @@ def listen_to_user():
         entry.insert(0, "Voice not recognized.")
     start_button.config(text="Start Talking")
 
+
 def clear_chat():
     chat_log.config(state="normal")
     chat_log.delete(1.0, "end")
     chat_log.config(state="disabled")
 
+
 def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 150)
-    engine.say(text)
-    engine.runAndWait()
+    tree = ET.parse("apikey.xml")
+    root = tree.getroot()
+
+    azure_api_key = root.find("azure_api_key").text.strip()
+    azure_region = root.find("azure_region").text.strip()
+
+    speech_config = SpeechConfig(subscription=azure_api_key, region=azure_region)
+    speech_config.speech_synthesis_voice_name = voice_combobox.get()
+
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)  
+    synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+    synthesizer.speak_text_async(text).get()
+
+
+
+def load_voices(api_key, region):
+    speech_config = SpeechConfig(subscription=api_key, region=region)
+    synthesizer = SpeechSynthesizer(speech_config=speech_config)
+
+    voices_result = synthesizer.get_voices_async().get()
+    voice_names = [voice.name for voice in voices_result.voices]
+
+    voice_combobox["values"] = voice_names
+    voice_combobox.current(0)
+
 
 root = tk.Tk()
 root.title("GhostGPT")
@@ -106,6 +156,9 @@ entry.grid(row=1, column=0, padx=5, pady=5)
 export_button = tk.Button(root, text="Export Chat", command=export_chat, bg="black", fg="white", width=10)
 export_button.grid(row=3, column=0, padx=5, pady=5)
 
+voice_combobox = ttk.Combobox(root, width=90)
+voice_combobox.grid(row=7, column=0, padx=5, pady=5)
+
 send_button = tk.Button(root, text="Send", command=send_message, bg="black", fg="white", width=10)
 send_button.grid(row=1, column=1, padx=5, pady=5)
 
@@ -118,6 +171,9 @@ clear_button.grid(row=2, column=1, padx=5, pady=5)
 api_key_button = tk.Button(root, text="API Key", command=create_and_open_api_key_file, bg="black", fg="white", width=10)
 api_key_button.grid(row=3, column=1, padx=5, pady=5)
 
+refresh_button = tk.Button(root, text="Refresh Voices", command=lambda: check_api_key_file(), bg="black", fg="white", width=12)
+refresh_button.grid(row=4, column=1, padx=5, pady=5)
+
 api_key_status = tk.Label(root, text="", bg="black")
 api_key_status.grid(row=4, column=0, padx=5, pady=5)
 
@@ -128,12 +184,8 @@ github_link = tk.Label(root, text="github.com/ghostkiwicoder", fg="cyan", cursor
 github_link.grid(row=6, column=0, padx=5, pady=0)
 github_link.bind("<Button-1>", open_github_link)
 
-
-
-
-
-
-
 check_api_key_file()
 
 root.mainloop()
+
+
